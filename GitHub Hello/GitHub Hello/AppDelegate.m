431ea7683ea9b32c8a00b6b3c2836ce914a17829
mantou <2287951815@qq.com>
2021-01-29 05:12:33 -0800

    change

--- GitHub Hello/GitHub Hello/AppDelegate.m
+++ GitHub Hello/GitHub Hello/AppDelegate.m
@@ -15,6 +15,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"=====");
     // Override point for customization after application launch.
     return YES;
 }
